7e46ff08ef2602de3f3139eab6034800aceba57e
Peter Wiebe <cmouyn@gmail.com>
2026-05-02 22:58:09 +0200

    import openai-java sdk and first functioning gpt-api-call

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -1,5 +1,20 @@
+import com.openai.client.OpenAIClient;
+import com.openai.client.okhttp.OpenAIOkHttpClient;
+import com.openai.models.responses.ResponseCreateParams;
+
 public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        OpenAIClient client = OpenAIOkHttpClient.fromEnv();
+
+        ResponseCreateParams params = ResponseCreateParams.builder()
+                .input("Just say Hi")
+                .model("gpt-5-nano")
+                .build();
+
+        client.responses().create(params).output().stream()
+                .flatMap(item -> item.message().stream())
+                .flatMap(message -> message.content().stream())
+                .flatMap(content -> content.outputText().stream())
+                .forEach(outputText -> System.out.println(outputText.text()));
     }
 }
